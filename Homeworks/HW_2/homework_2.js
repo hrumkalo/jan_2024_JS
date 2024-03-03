ce40db16dcc61db6b96a7f39dd55475e459b711c
hrumkalo <Ukrainian.Falcon@gmail.com>
2024-03-03 23:16:45 +0200

    hw_2

---
+++ Homeworks/HW_2/homework_2.js
@@ -0,0 +1,36 @@
+<!--Масиви та об'єкти:-->
+// ---------------------------------------1.1---------------------------------------
+
+// let NamesAndNumbers= ["zlata", 5, "mariia", 6, "victoriia", 9, "nina", 4, "kristina", 8];
+// console.log(NamesAndNumbers.length);
+// console.log(NamesAndNumbers [0]);
+// console.log(NamesAndNumbers [1]);
+// console.log(NamesAndNumbers [2]);
+// console.log(NamesAndNumbers [3]);
+// console.log(NamesAndNumbers [4]);
+// console.log(NamesAndNumbers [5]);
+// console.log(NamesAndNumbers [6]);
+// console.log(NamesAndNumbers [7]);
+// console.log(NamesAndNumbers [8]);
+// console.log(NamesAndNumbers [9]);
+
+// ---------------------------------------1.2---------------------------------------
+
+// let books = {
+//     title: "The Wolf in th eWatchtower",
+//     pageCount: 447,
+//     genre: "fantasy"
+// }
+// console.log(books);
+
+// ---------------------------------------1.3---------------------------------------
+
+let firstBook = {title: "Holly", pageCount: 464, genre: "horror",
+    authors:["Stephen King", 76]};
+let secondBook = {title: "Harry Potter and the Philosopher's Stone", pageCount: 368, genre: "fantasy",
+    authors:["Joanne Rowling", 58]};
+let thirdBook = {title: "Fourth Wing", pageCount: 512, genre: "horror",
+    authors:["Stephen King", 76]};
+
+
+console.log(firstBook)
